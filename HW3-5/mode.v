279bcba03862b39c5a014a0315ce97ba586419ca
carloschen1126 <carlos79103@gmail.com>
2021-12-03 22:01:43 +0800

    hw3 init

---
+++ HW3-5/mode.v
@@ -0,0 +1,10 @@
+`timescale 1ns/1ps
+
+module mode_max(mode, i0, i1, i2, i3, i4);
+//DO NOT CHANGE!
+	input  [3:0] i0, i1, i2, i3, i4;
+	output [3:0] mode;
+//---------------------------------------------------
+
+
+endmodule
